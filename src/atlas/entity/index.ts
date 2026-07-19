dcaab4c932fb39bb4c317a5c4c912abb62dc499b
boshitrdhk <145635395+boshitrdhk@users.noreply.github.com>
2026-07-19 15:12:54 -0400

    feat(atlas): export entity engine API

---
+++ src/atlas/entity/index.ts
@@ -0,0 +1,6 @@
+export * from "./Entity";
+export * from "./EntityFactory";
+export * from "./EntityRepository";
+export * from "./EntityTypes";
+export * from "./EntityValidator";
+export * from "./InMemoryEntityRepository";
