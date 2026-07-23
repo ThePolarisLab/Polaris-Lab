a5385386484b4175079e7a912cffefe8bceeeff6
boshitrdhk <145635395+boshitrdhk@users.noreply.github.com>
2026-07-23 15:51:46 -0400

    feat(runtime): export lifecycle API

---
+++ src/runtime/lifecycle/index.ts
@@ -0,0 +1,6 @@
+export * from "./LifecycleManager";
+export * from "./LifecycleRecord";
+export * from "./LifecycleSnapshot";
+export * from "./RuntimeEvents";
+export * from "./TransitionValidator";
+export * from "./WorkerState";
